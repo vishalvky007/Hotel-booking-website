16ead217a32568db0eb6570d9a993b8749269ae0
vishalvky007 <9512278k@gmail.com>
2024-01-26 20:14:51 +0530

    modified

--- index.php
+++ index.php
@@ -59,18 +59,27 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <form>
+          <!-- Modal Header -->
           <div class="modal-header">
             <h5 class="modal-title d-flex align-items-center">
               <i class="bi bi-person-circle fs-3 me-2"></i>User Login
             </h5>
             <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          
+          <!-- Modal Body -->
           <div class="modal-body">
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control shadow-none">
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control shadow-none">
+            </div>
+            <div>
+              <button>
+                Login
+              </button>
             </div>
           </div>
         </form>
