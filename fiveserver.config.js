35526c5627e8df5744725dd3c4b3b6c800d33287
vishalvky007 <9512278k@gmail.com>
2024-01-27 00:59:36 +0530

    fiveserver file modified

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,4 +1,11 @@
 // fiveserver.config.js
 module.exports = {
-    php: "/usr/bin/php"              // macOS/Ubuntu
+    php: "/usr/bin/php",             // macOS/Ubuntu
+    open: "index.php",
+    injectBody: true,     // enable live reloading
+    remoteLogs: true,
+    remoteLogs: "yellow",
+    injectCss: true,
+    navigate: true,
+    // highlight: true,
 } 
