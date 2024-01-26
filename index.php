080dcf3f823ea70139ec19aed143a722f8becc3b
vishalvky007 <9512278k@gmail.com>
2024-01-27 00:39:49 +0530

    modified index.php

--- index.php
+++ index.php
@@ -16,10 +16,20 @@
     .h-font {
       font-family: 'Merienda', cursive;
     }
+
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      margin: 0;
+    }
+    
   </style>
 </head>
 
 <body>
+
   <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
       <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Paradise</a>
@@ -45,9 +55,13 @@
           </li>
         </ul>
         <div class="d-flex">
-          <!-- Button trigger modal -->
-          <button type="button" class="btn btn-outline-dark me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+          <!-- Button trigger Login modal -->
+          <button type="button" class="btn btn-outline-dark me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
             Login
+          </button>
+          <!-- Button trigger Register modal -->
+          <button type="button" class="btn btn-outline-dark me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#registerModal">
+            Register
           </button>
         </div>
       </div>
@@ -72,15 +86,83 @@
               <label class="form-label">Email address</label>
               <input type="email" class="form-control shadow-none">
             </div>
-            <div class="mb-3">
+            <div class="mb-4">
               <label class="form-label">Password</label>
               <input type="password" class="form-control shadow-none">
             </div>
-            <div>
-              <button>
-                Login
-              </button>
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              <button type="submit" class="btn btn-primary">Login</button>
+              <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forget Passoword?</a>
             </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <!-- Register Modal -->
+  <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <form>
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h5 class="modal-title d-flex align-items-center">
+              <i class="bi bi-person-add fs-3 me-3"></i>User Registration
+            </h5>
+            <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <!-- Modal Body -->
+          <div class="modal-body">
+            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+              Note: Your details must match with your ID (Aadhaar-card, Passport, Driving-license, etc.)
+              that will be required during Check-in.
+            </span>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Name</label>
+                  <input type="email" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0">
+                  <label class="form-label">Email</label>
+                  <input type="email" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Phone Number</label>
+                  <input type="number" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Picture</label>
+                  <input type="file" class="form-control shadow-none">
+                </div>
+                <div class="col-md-12 p-0 mb-3">
+                  <label class="form-label">Address</label>
+                  <textarea class="form-control" row="2"></textarea>
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Pincode</label>
+                  <input type="number" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Date of Birth</label>
+                  <input type="date" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Confirm Password</label>
+                  <input type="password" class="form-control shadow-none">
+                </div>
+
+              </div>
+            </div>
+            <div class="text-center my-1">
+              <button type="submit" class="btn btn-success">Register</button>
+            </div>
+
           </div>
         </form>
       </div>
