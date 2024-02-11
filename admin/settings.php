19023da0ee9d7aa4e67d7f669fbcf69a0eeda637
vishalvky007 <9512278k@gmail.com>
2024-02-12 01:05:58 +0530

    shutdown added

--- admin/settings.php
+++ admin/settings.php
@@ -1,6 +1,6 @@
 <?php
-  require("include/essentials.php");
-  adminLogin();
+require("include/essentials.php");
+adminLogin();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +22,7 @@
         <h3 class="mb-4">SETTINGS</h3>
 
         <!-- General settings sections -->
-        <div class="card">
+        <div class="card border-0 shadow-sm mb-4">
           <div class="card-body">
             <div class="d-flex align-items-center justify-content-between mb-3">
               <h5 class="card-title m-0">General Settings</h5>
@@ -58,10 +58,28 @@
                 </div>
                 <div class="modal-footer">
                   <button type="button" onclick="site_title.value = general_data.site_title, site_about.value = general_data.site_about" class="btn text-secondary border border-outline-dark" data-bs-dismiss="modal">CANCEL</button>
-                  <button type="button" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                  <button type="button" onclick="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">SUBMIT</button>
                 </div>
               </div>
             </form>
+          </div>
+        </div>
+
+        <!-- Shutdown section -->
+
+        <div class="card border-0 shadow-sm">
+          <div class="card-body">
+            <div class="d-flex align-items-center justify-content-between mb-3">
+              <h5 class="card-title m-0">Shutdown Website</h5>
+              <div class="form-check form-switch">
+                <form>
+                  <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox" id="shutdown-toggle">
+                </form>
+              </div>
+            </div>
+            <p class="card-text">
+              No customers will be allowed to book room, when shutdown mode is turned on.
+            </p>
           </div>
         </div>
 
@@ -81,8 +99,10 @@
       let site_title_inp = document.getElementById('site_title_inp');
       let site_about_inp = document.getElementById('site_about_inp');
 
+      let shutdown_toggle = document.getElementById('shutdown-toggle');
+
       let xhr = new XMLHttpRequest();
-      xhr.open('POST','ajax/settings_crud.php', true);
+      xhr.open('POST', 'ajax/settings_crud.php', true);
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
       xhr.onload = function() {
@@ -93,10 +113,60 @@
 
         site_title_inp.value = general_data.site_title;
         site_about_inp.value = general_data.site_about;
+
+        if (general_data.shutdown == 0) {
+          shutdown_toggle.checked = false;
+          shutdown_toggle.value = 0;
+        } else {
+          shutdown_toggle.checked = true;
+          shutdown_toggle.value = 1;
+        }
       }
 
       xhr.send('get_general');
     }
+
+    function upd_general(site_title_val, site_about_val) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "ajax/settings_crud.php", true);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+      xhr.onload = function() {
+
+        var myModal = document.getElementById('general-s');
+        var modal = bootstrap.Modal.getInstance(myModal);
+        modal.hide();
+
+        if (this.responseText == 1) {
+          alert('success', 'Changes saved!');
+          get_general();
+        } else {
+          alert('error', 'No changes made!');
+        }
+
+      }
+
+      xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&upd_general');
+    }
+
+    function upd_shutdown(val) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "ajax/settings_crud.php", true);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+      xhr.onload = function() {
+
+        if (this.responseText == 1 && general_data.shutdown == 0) {
+          alert('success', 'Site has been Shutdown!');
+        } else {
+          alert('success', ' Shutdown mode off!');
+        }
+        get_general();
+      }
+
+      xhr.send('upd_shutdown='+val);
+    }
+
 
     window.onload = function() {
       get_general();
